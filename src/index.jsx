4a40aa3e27a4631cb8cf1cd0ca8235349047b9f3
Bomtek9 <clayduplantis9@gmail.com>
2023-09-25 13:11:23 -0500

    udpdated dev dependencies

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import { createRoot } from 'react-dom/client';
+
+// Import statement to indicate that I need to bundle `./index.scss`
+import "./index.scss";
+
+// Main component (will eventually use all the others)
+constMyFlixApplication = () => {
+    return (
+        <div className = "my-flix">
+            <div> Good morning </div>
+        </div>
+    );
+};
+
+//finds the root of the app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+// Tells React to render the app in the root DOM element
+root.render(<MyFlixApplication />);
+
+
+

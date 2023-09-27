7e269935fcc5f0e2ca44c60cdfaa28bb272bf5fd
Bomtek9 <clayduplantis9@gmail.com>
2023-09-27 15:47:46 -0500

    Added Images

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27326,21 +27326,21 @@ const MainView = ()=>{
             id: "64f22dad90ebac5592caf568",
             Name: "It's A Wonderful Life",
             Description: "An angel is sent from Heaven to help a desperately frustrated businessman, George Bailey, by showing him what life would have been like if he had never existed.",
-            ImagePath: "itsawonderfullife.png",
+            ImagePath: "https://is1-ssl.mzstatic.com/image/thumb/Video49/v4/9b/f1/5b/9bf15b1d-1a2f-7fb3-c40a-1c0a1d8322ad/source/1200x630bb.jpg",
             Featured: "true"
         },
         {
             id: "64f2316590ebac5592caf56a",
             Name: "Teenage Mutant Ninja Turtles",
             Description: "Four teenage mutant ninja turtles Leonardo, Raphael, Donatello, and Michaelangelo fight crime in New York City from the shadows.",
-            ImagePath: "teenagemutantninjaturtles.png",
+            ImagePath: "https://www.screengeek.net/wp-content/uploads/2019/03/teenage-mutant-ninja-turtles.jpg",
             Featured: "true"
         },
         {
             id: "64f21b4f90ebac5592caf561",
             Name: "Interstellar",
             Description: "When Earth becomes uninhibitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
-            ImagePath: "interstellar.png",
+            ImagePath: "https://www.cinemahub.me/wp-content/uploads/2018/06/46590hd.jpg",
             Featured: "true"
         }
     ]);
@@ -27377,7 +27377,7 @@ const MainView = ()=>{
         columnNumber: 9
     }, undefined);
 };
-_s(MainView, "H+RVkPTQitan0iOlHF9+LydwzQ0=");
+_s(MainView, "X+To09rUWqeAgmqNCqlsAdVU1kQ=");
 _c = MainView;
 var _c;
 $RefreshReg$(_c, "MainView");
@@ -27403,7 +27403,7 @@ const MovieView = ({ movie, onBackClick })=>{
         children: [
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
                 children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
-                    src: movie.image
+                    src: movie.ImagePath
                 }, void 0, false, {
                     fileName: "src/components/movie-view/movie-view.jsx",
                     lineNumber: 5,
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,14 +9,14 @@ export const MainView = () => {
             id: "64f22dad90ebac5592caf568",
             Name: "It's A Wonderful Life",
             Description: "An angel is sent from Heaven to help a desperately frustrated businessman, George Bailey, by showing him what life would have been like if he had never existed.",
-            ImagePath: "itsawonderfullife.png",
+            ImagePath: "https://is1-ssl.mzstatic.com/image/thumb/Video49/v4/9b/f1/5b/9bf15b1d-1a2f-7fb3-c40a-1c0a1d8322ad/source/1200x630bb.jpg",
             Featured: "true"
         },
         {
             id: "64f2316590ebac5592caf56a",
             Name: "Teenage Mutant Ninja Turtles",
             Description: "Four teenage mutant ninja turtles Leonardo, Raphael, Donatello, and Michaelangelo fight crime in New York City from the shadows.",
-            ImagePath: "teenagemutantninjaturtles.png",
+            ImagePath: "https://www.screengeek.net/wp-content/uploads/2019/03/teenage-mutant-ninja-turtles.jpg",
             Featured: "true"
         },
 
@@ -24,7 +24,7 @@ export const MainView = () => {
             id: "64f21b4f90ebac5592caf561",
             Name: "Interstellar",
             Description: "When Earth becomes uninhibitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
-            ImagePath: "interstellar.png",
+            ImagePath: "https://www.cinemahub.me/wp-content/uploads/2018/06/46590hd.jpg",
             Featured: "true"
         }
     ]);

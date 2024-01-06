e458a4c4c69cebfddc1e9eee2044a5aa33c36708
Bomtek9 <clayduplantis9@gmail.com>
2024-01-06 15:49:57 -0600

    REverting

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -20,34 +20,6 @@ export const SignupView = () => {
   const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
-  const formatBirthday = (rawDate) => {
-    try {
-      // Split the date components
-      const [month, day, year] = rawDate.split("/");
-
-      // Log the components to check if they are correct
-      console.log("Month:", month);
-      console.log("Day:", day);
-      console.log("Year:", year);
-
-      // Create a new Date object using the components
-      const dateObject = new Date(`${year}-${month}-${day}`);
-
-      // Check if the dateObject is a valid date
-      if (isNaN(dateObject.getTime())) {
-        throw new Error("Invalid date");
-      }
-
-      // Format the date to match the backend's expected format (YYYY-MM-DD)
-      const formattedDate = dateObject.toISOString().split("T")[0];
-
-      return formattedDate;
-    } catch (error) {
-      console.error("Error formatting birthday:", error);
-      throw error;
-    }
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -69,7 +41,7 @@ export const SignupView = () => {
       Username: username,
       Password: password,
       Email: email,
-      Birthday: formatBirthday(birthday),
+      Birthday: birthday,
     };
 
     fetch("https://dup-movies-18ba622158fa.herokuapp.com/users", {
